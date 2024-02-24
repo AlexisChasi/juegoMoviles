1993144d6d8ac30f6f88ebd25926aa4ee266865a
Franklin Patiño <jhoelpatino26@hotmail.com>
2024-02-23 22:51:05 -0500

    Firestore

--- android/src/com/shooter/game/AndroidLauncher.java
+++ android/src/com/shooter/game/AndroidLauncher.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.shooter.game.SpaceShooterGame;
+import com.shoter.game.core.SpaceShooterGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
